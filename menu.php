54519584ebad9c33c84f4d17f75024b324536ee0
skychristoph <sky.christoph@gmail.com>
2025-08-30 10:52:41 -0400

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,61 @@
+<nav position="left">
+            <h1 class="title"><a  href="https://noiseless-mammoth-slouch.glitch.me/">Sky Christoph</a></h1>
+            <h1 class="body">Projects</h1>
+
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/CassetteDesign.html">
+              <button type="submit">Cassette Design</button>
+              </form>
+            </p>
+  
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/PosterDesignWork.html">
+              <button type="submit">Poster/Design Work</button>
+              </form>
+            </p>
+            
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/Kingfisher.html">
+              <button type="submit">Kingfisher</button>
+              </form>
+            </p>
+
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/FiveSecondsFromFifteen.html">
+              <button type="submit">Five Seconds From Fifteen</button>
+              </form>
+            </p>
+            
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/NoPlaceOneTakes.html">
+              <button type="submit">No Place One Takes</button>
+              </form>
+            </p>
+            
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/UsSeptemberchildren.html">
+              <button type="submit">Us Septemberchildren</button>
+              </form>
+            </p>
+            
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/LODTAA.html">
+              <button type="submit">LODTAA &amp; In (Thirds/Threes)</button>
+              </form>
+            </p>
+          
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/Out___.html">
+              <button type="submit">Out___</button>
+              </form>
+            </p>
+            
+            <p>
+              <form action="https://noiseless-mammoth-slouch.glitch.me/CamFeedtotheSky.html">
+              <button id="CFttS" type="submit">CamFeed to the Sky</button>
+              </form>
+            </p>
+  
+
+            <p><a href="https://vimeo.com/skychristoph2" target="_blank">Vimeo</a></p>
+          </nav>
